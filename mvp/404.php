50856a2825e5aa3f4a46cc20f5a1a30dc8c44e3b
Georgi Marokov <georgi.marokov@gmail.com>
2017-12-02 17:40:57 +0200

    404 page

--- mvp/404.php
+++ mvp/404.php
@@ -12,12 +12,12 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
+			<section class="section error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'mvp' ); ?></h1>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
+				<div class="section-inner">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'mvp' ); ?></p>
 
 					<?php
